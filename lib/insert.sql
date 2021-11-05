76dcc90797f9f8e6fd8393b35c4168e996b5b7f1
Leo Chen <chenl3@rose-hulman.edu>
2021-11-04 17:42:10 -0700

    commit tree 3503fc55588e2bb14687b77b0843441b78fdf3b7

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Mr. Chocolate",
+  10,
+  "M",
+  "Brown",
+  "Friendly",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Rowdy",
+  3,
+  "M",
+  "Black & White",
+  "Rowdy",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Tabitha",
+  7,
+  "F",
+  "White",
+  "Curious",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Sergeant Brown",
+  11,
+  "M",
+  "Brown",
+  "Determined",
+  false
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Melissa",
+  1,
+  "F",
+  "White",
+  "Playful",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Grinch",
+  13,
+  "M",
+  "Green",
+  "Mean",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  "Wendy",
+  5,
+  "F",
+  "Black & White",
+  "Hungry",
+  true
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+  NULL,
+  1,
+  "M",
+  "Brown",
+  "Timid",
+  true
+);
